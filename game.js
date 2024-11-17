499334ec0b2c99d049bdb7e92c4d8d4e3e026b9d
ryanlester91 <lestertr@dukes.jmu.edu>
2024-11-16 19:43:48 -0500

    shrunk cards for better display

--- game.js
+++ game.js
@@ -51,14 +51,36 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
+const cardsChosenIds = []
 
 function createBoard() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/zorak.jpg')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function checkMatch(){
+    const cards = document.querySelectorAll('img')
+    console.log('check for match!')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        //cards[cardsChosenIds[0]].setAttribute('src', images/white.png)]
+    }
+}
+
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
